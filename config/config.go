a6c1b10b807e407663438b0b046b1fa3324c622d
vitorcontiz <vitorcontizgit@gmail.com>
2023-02-21 11:40:50 -0300

    feat: create config with viper

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package configs
+
+import (
+	"github.com/spf13/viper"
+)
+
+var cfg *conf
+
+type conf struct {
+	AwsRegion    string `mapstructure:"AWS_REGION"`
+	AwsKey       string `mapstructure:"AWS_KEY"`
+	AWS_PASSWORD string `mapstructure:"AWS_PASSWORD"`
+}
+
+func LoadConfig(path string) (*conf, error) {
+	viper.SetConfigName("app_config")
+	viper.SetConfigType("env")
+	viper.AddConfigPath(path)
+	viper.SetConfigFile(".env")
+	viper.AutomaticEnv()
+	err := viper.ReadInConfig()
+	if err != nil {
+
+		panic(err)
+	}
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	return cfg, err
+}
